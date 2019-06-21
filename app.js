0f1a1f9064280730577585d0f7fef79f1e666551
Jamie Lipschitz <jay.lipschitz@gmail.com>
2019-06-21 17:36:14 -0400

    add implementation comments
    gathering input, calculator and returning value amount

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+/*
+Objective: Gather user input and then return amount owed per person(s)
+
+Rough Draft:
+import calculator 
+let bill = {}
+let prompts = {}
+
+Greeting - "Welcome to the bill splitter!"
+
+    1) Get the bill amount - askBillAmount()
+        1a) value > 0 ? bill.amount = value : askBillAmount(-#)
+    2) Get the tip amount - askTipAmount()
+        2a) value > 0 ?  bill.tip = percentage: askTipValue(-#)
+    3) "Do you want to split the bill?""
+        3a) True ? (Prompt how many) : Continue()
+             3b) "How many are you splitting with?" -- If true
+                3c) val > 0 bill.people = value : askPeopleValue(-#)
+
+return 
+    bill.people ? calculator(val, %, people) : calculator(val, %)
+*/
---
+++ calculator.js
@@ -0,0 +1,10 @@
+/*
+    Build calculator to compute amount owed on bill
+    Split bill amount if there are multiple people
+
+    const calculator = (val, %, people) => {
+
+    }
+
+    module.exports = calculator
+*/
