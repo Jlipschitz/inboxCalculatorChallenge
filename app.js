58fc60f1556e0a3e7945f2f289f76242f989fff9
Jamie Lipschitz <jay.lipschitz@gmail.com>
2019-06-22 19:20:14 -0400

    more input + bill
    greetinsg + inputs and calculate

--- app.js
+++ app.js
@@ -1,53 +1,24 @@
-//initiate our cli
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+const prompt = require('./prompt.js');
 
-let prompt = {
-    //simple text used for the cli and user input functions
-    text: {
-        greetings: {
-            welcome: 'Hello, welcome to Bill Calculator! \n',
-            goodbye: 'Thank you for using Bill Calculator! \n'
-        },
-        queries: {
-            bill: 'What is your bill amount? \n',
-            tip: 'How much would you like to tip? \n',
-            split: 'Are you splitting the bill with anyone? \n',
-            splitCount: 'How many will you be splitting with? \n'
-        },
-        errors: {
-            0: 'Sorry, number must be higher than 0. Please try again. \n',
-            1: 'Sorry, that is not a valid number. Please try again. \n'
-        }
-    },
-    ask: (err, type, cb) => {
-        //gather user input from command line
-        readline.question(!err ? prompt.text.queries[type] : err, (value) => {
+console.log(prompt.text.greetings.welcome);
 
-            //convert user input to a float and strip away special characters
-            value = parseFloat(
-                value.replace(/[`~!@#$%^&*()_|+\-=?;:'", <>\{\}\[\]\\\/]/gi, '')
-            );
+//invoke our ask functions to gather user input 
+prompt.ask(null, 'bill', (totalBillValue) => {
+    prompt.ask(null, 'tip', (totalTipPercentage) => {
+        prompt.ask(null, 'split', (totalSplitValue) => {
 
-            //error handling to make sure the bill amount is usable
-            //TODO: Add handling for people question
-            if (value < 0) {
-                return prompt.query(prompt.text.errors[0], cb)
-            } else if (isNaN(value)) {
-                return prompt.ask(prompt.text.errors[1], cb)
-            }
+            //after we've gathered inputs do the bill maths
+            let totalOwed = totalSplitValue > 0 ? (totalBillValue + ((totalTipPercentage / 100) * totalBillValue)) / totalSplitValue
+                : (totalBillValue + ((totalTipPercentage / 100) * totalBillValue))
 
-            return cb(value);
+            //display amount the person(s) owe
+            totalSplitValue > 0 ? console.log(`You each owe ${totalOwed}`)
+                : console.log(`You owe ${totalOwed}`)
 
+            //wave goodbye!    
+            console.log(prompt.text.greetings.goodbye)
+
+            prompt.close();
         })
-    }
-};
-
-//invoke our ask functions to gather user input TODO: Nest functions?
-prompt.ask(null, 'bill', (value) => {
-    console.log(`Your bill amount is: ${value}`)
-    readline.close()
-    process.stdin.destroy();
+    })
 })
---
+++ prompt.js
@@ -0,0 +1,52 @@
+//initiate our cli
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+prompt = {
+    //simple text used for the cli and user input functions
+    text: {
+        greetings: {
+            welcome: 'Hello, welcome to Bill Calculator! \n',
+            goodbye: 'Thank you for using Bill Calculator! \n'
+        },
+        queries: {
+            bill: 'What is your bill amount? \n',
+            tip: 'What tip percentage would you like to put? \n',
+            split: 'How many will be splitting this bill? Please enter 0 if none.\n'
+        },
+        errors: {
+            0: 'Sorry, number must be higher than 0. Please try again. \n',
+            1: 'Sorry, that is not a valid number. Please try again. \n'
+        }
+    },
+    ask: (err, type, cb) => {
+        //gather user input from command line
+        readline.question(!err ? prompt.text.queries[type] : err, (value) => {
+
+            //convert user input to a float and strip away special characters
+            value = parseFloat(
+                value.replace(/[`~!@#$%^&*()_|+\-=?;:'", <>\{\}\[\]\\\/]/gi, '')
+            );
+
+            //error handling to make sure the bill amount is usable
+            //TODO: Add handling for people question
+            if (value < 0) {
+                return prompt.query(prompt.text.errors[0], cb)
+            } else if (isNaN(value)) {
+                return prompt.ask(prompt.text.errors[1], cb)
+            }
+
+            return cb(value);
+
+        })
+    },
+    close: () => {
+        //end tty stream otherwise it will continuously run
+        readline.close()
+        process.stdin.destroy();
+    }
+};
+
+module.exports = prompt;
