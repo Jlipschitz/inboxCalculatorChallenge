603803fb74e1d8eda48dbb00c3dc8e02d045f100
Jamie Lipschitz <jay.lipschitz@gmail.com>
2019-06-22 18:40:06 -0400

    removed fluff
    added prompt and took out excess file/comments

--- app.js
+++ app.js
@@ -1,24 +1,53 @@
+//initiate our cli
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
+let prompt = {
+    //simple text used for the cli and user input functions
+    text: {
+        greetings: {
+            welcome: 'Hello, welcome to Bill Calculator! \n',
+            goodbye: 'Thank you for using Bill Calculator! \n'
+        },
+        queries: {
+            bill: 'What is your bill amount? \n',
+            tip: 'How much would you like to tip? \n',
+            split: 'Are you splitting the bill with anyone? \n',
+            splitCount: 'How many will you be splitting with? \n'
+        },
+        errors: {
+            0: 'Sorry, number must be higher than 0. Please try again. \n',
+            1: 'Sorry, that is not a valid number. Please try again. \n'
+        }
+    },
+    ask: (err, type, cb) => {
+        //gather user input from command line
+        readline.question(!err ? prompt.text.queries[type] : err, (value) => {
 
-/*
-Objective: Gather user input and then return amount owed per person(s)
+            //convert user input to a float and strip away special characters
+            value = parseFloat(
+                value.replace(/[`~!@#$%^&*()_|+\-=?;:'", <>\{\}\[\]\\\/]/gi, '')
+            );
 
-Rough Draft:
-import calculator 
-let bill = {}
-let prompts = {}
+            //error handling to make sure the bill amount is usable
+            //TODO: Add handling for people question
+            if (value < 0) {
+                return prompt.query(prompt.text.errors[0], cb)
+            } else if (isNaN(value)) {
+                return prompt.ask(prompt.text.errors[1], cb)
+            }
 
-Greeting - "Welcome to the bill splitter!"
+            return cb(value);
 
-    1) Get the bill amount - askBillAmount()
-        1a) value > 0 ? bill.amount = value : askBillAmount(-#)
-    2) Get the tip amount - askTipAmount()
-        2a) value > 0 ?  bill.tip = percentage: askTipValue(-#)
-    3) "Do you want to split the bill?""
-        3a) True ? (Prompt how many) : Continue()
-             3b) "How many are you splitting with?" -- If true
-                3c) val > 0 bill.people = value : askPeopleValue(-#)
+        })
+    }
+};
 
-return 
-    bill.people ? calculator(val, %, people) : calculator(val, %)
-*/
+//invoke our ask functions to gather user input TODO: Nest functions?
+prompt.ask(null, 'bill', (value) => {
+    console.log(`Your bill amount is: ${value}`)
+    readline.close()
+    process.stdin.destroy();
+})
