77d08ddda32a9cefa03184240b126b3a5bb111d4
Jamie Lipschitz <jay.lipschitz@gmail.com>
2019-06-29 21:16:21 -0400

    display per person on initial output

--- app.js
+++ app.js
@@ -10,16 +10,23 @@ const startApplication = () => {
             prompt.getInput(null, 'split', (totalSplitValue) => {
 
                 //do maths on the amount inputs gathered from user
+                let checkSplit = () => { return totalSplitValue >= 2 ? true : false }
+
                 let amount = {
                     bill: totalBillValue,
-                    tip: totalBillValue * (totalTipPercentage / 100).toFixed(2),
-                    split: totalSplitValue >= 2 ? totalSplitValue : 0,
-                    total: totalSplitValue >= 2 ? parseFloat((totalBillValue + ((totalTipPercentage / 100) * totalBillValue)) / totalSplitValue).toFixed(2) :
-                        parseFloat(totalBillValue + ((totalTipPercentage / 100) * totalBillValue)).toFixed(2)
+                    tip: (totalBillValue * (totalTipPercentage / 100)).toFixed(2),
+                    split: checkSplit() ? totalSplitValue : 0,
+                    total: checkSplit() ? ((totalBillValue + ((totalTipPercentage / 100) * totalBillValue)) / totalSplitValue).toFixed(2)
+                        : (totalBillValue + ((totalTipPercentage / 100) * totalBillValue)).toFixed(2)
                 }
 
                 //show the amounts that the person(s) owe
-                console.log(`\nYou ${totalSplitValue >= 2 ? 'each owe' : 'owe'}: $${amount.total}.`)
+                console.log(
+                    `\nBill: $${(checkSplit() ? (amount.bill / totalSplitValue) : amount.bill).toFixed(2)} + `
+                    + `%${totalTipPercentage} `
+                    + `Tip: $${(checkSplit() ? (amount.tip / totalSplitValue).toFixed(2) : amount.tip)}`
+                );
+                console.log(`You ${checkSplit() ? 'each owe' : 'owe'}: $${amount.total}.`);
 
                 //store the bill caculated and ask user if he would like to calculate another bill
                 prompt.recentBills.push({ amount })
--- prompt.js
+++ prompt.js
@@ -9,8 +9,10 @@ prompt = {
         questions: {
             bill: '\nWhat is your bill amount? \n',
             tip: '\nWhat tip percentage would you like to tip? \n',
-            split: '\nHow many will be splitting this bill? Please enter 0 if none.\n'
-        }
+            split: '\nHow many will be splitting this bill? Please enter 0 if none.\n',
+            additionalBill: '\nWould you like to calculate another bill? \n'
+        },
+        invalidInput: 'Sorry, I did not understand that. Try again. \n'
     },
     recentBills: [],
     getInput: (err, type, callback) => {
@@ -19,24 +21,28 @@ prompt = {
             let cleanUserInput = parseFloat(
                 userInput.replace(/[`~!@#$%^&*()_|+\=?;:'", <>\{\}\[\]\\\/]/gi, '')
             );
-+6
+
+            let lowNumberText = `Your ${type} must be higher than 0. Try again. \n`
+
             //show incorrect input text to user otherwise return input value
             if (cleanUserInput < 0) {
-                prompt.getInput(`Sorry, your ${type} must be higher than 0. Try again. \n`, type, callback)
+                prompt.getInput(lowNumberText, type, callback)
             } else if (isNaN(cleanUserInput)) {
-                return prompt.getInput('Sorry, I did not understand that. Try again. \n', type, callback);
+                return prompt.getInput(prompt.text.invalidInput, type, callback);
             } else if (cleanUserInput === 0 && (type === 'bill' || type === 'tip')) {
                 return type !== 'tip' ?
-                    prompt.getInput(`Sorry, your ${type} must be higher than 0. Try again. \n`, type, callback) :
-                    prompt.getInput(`Don't be cheap! Your ${type} must be higher than 0. Try again. \n`, type, callback);
+                    prompt.getInput(lowNumberText, type, callback) :
+                    prompt.getInput(`Don't be cheap! ${lowNumberText}`, type, callback);
             } else {
                 return callback(cleanUserInput);
             }
         })
     },
     closeApplication: (application) => {
+        let option = 'Type [Y]es to continue || [N]o to exit|| [V]iew to show recent bills \n'
+
         //ask user if he would like calculate another bill or show recent bills and terminate tty stream if not
-        readline.question('\nWould you like to calculate another bill? \nType [Y]es to continue || [N]o to exit|| [V]iew to show recent bills \n', (userInput) => {
+        readline.question(prompt.text.questions.additionalBill + option, (userInput) => {
             if (userInput.toLocaleLowerCase() === 'yes' || userInput.toLocaleLowerCase() === 'y') {
                 application();
             } else if (userInput.toLocaleLowerCase() === 'no' || userInput.toLocaleLowerCase() === 'n') {
@@ -55,7 +61,7 @@ prompt = {
                 prompt.closeApplication(application);
             } else {
                 //ask user again if input is otherwise
-                console.log('\nSorry, I did not understand that.')
+                console.log(prompt.text.invalidInput)
                 prompt.closeApplication(application);
             }
         })
